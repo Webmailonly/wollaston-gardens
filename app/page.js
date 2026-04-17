9e421418821a98cd5b0c4043b63700ad506e1ca0
Webmailonly <webmailonly702@gmail.com>
2026-04-16 21:12:22 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1063,9 +1063,7 @@ export default function Page() {
                 <a href="#booking" className="btn btn-primary">
                   Request a Booking
                 </a>
-    <a href="/why-wollaston-gardens" className="btn btn-secondary">
-  Why Wollaston Gardens
-</a>
+    
               </div>
             </div>
           </div>
